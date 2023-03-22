dac1fc1176c086bc2ed0b2b3996d454a1c8fc941
jonJJ2016 <jonmartirosyan1988@gmail.com>
2023-03-23 02:26:27 +0800

    aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve('./src/'),
-            '@features': path.resolve(__dirname, './src/features/'),
-            '@layout': path.resolve(__dirname, './src/app/layout/'),
-            '@pages': path.resolve(__dirname, './src/pages/'),
-            '@app': path.resolve(__dirname, './src/app/'),
-            '@shared': path.resolve(__dirname, './src/shared/'),
-            '@assets': path.resolve(__dirname, './src/assets/'),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src/'),
+      '@features': path.resolve(__dirname, './src/features/'),
+      '@layout': path.resolve(__dirname, './src/app/layout/'),
+      '@pages': path.resolve(__dirname, './src/pages/'),
+      '@app': path.resolve(__dirname, './src/app/'),
+      '@shared': path.resolve(__dirname, './src/shared/'),
+      '@assets': path.resolve(__dirname, './src/assets/'),
     },
+  },
 })
